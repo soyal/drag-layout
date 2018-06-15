e9188bcc59ea8cbdf629c30c3981707d59bc55b6
soyal <xiongxiao9958@gmail.com>
2018-06-15 15:26:23 +0800

    fold init

---
+++ src/app.jsx
@@ -0,0 +1,14 @@
+import React, { Component } from 'react'
+import Item from './components/item'
+
+class App extends Component {
+  render() {
+    return (
+      <div className="app">
+        <Item />
+      </div>
+    )
+  }
+}
+
+export default App
---
+++ src/components/item/index.jsx
@@ -0,0 +1,13 @@
+/**
+ * 可拖拽，拉伸的单个item
+ */
+import React, { PureComponent } from 'react'
+import './index.less'
+
+class Item extends PureComponent {
+  render() {
+    return <div className="drag-layout__item">item</div>
+  }
+}
+
+export default Item
